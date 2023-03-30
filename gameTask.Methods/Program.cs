fdd235cccd5496cb59836461aa6fa79563e3606e
Dell <ersadehmedov01@gmail.com>
2023-03-30 09:19:03 +0300

    Add project files.

---
+++ gameTask.Methods/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+namespace gameTask.Methods
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    //task9
+        //    int number = 2;
+        //    int power = 3;
+        //    int result = Power(number, power);
+        //    Console.WriteLine($"{number}^{power} = {result}"); 
+        //}
+
+        //static int Power(int number, int power)
+        //{
+        //    int result = 1;
+        //    for (int i = 1; i <= power; i++)
+        //    {
+        //        result *= number;
+        //    }
+        //    return result;
+        //}
+
+
+
+        //task8
+        //static void Main(string[] args)
+        //{
+        //    int param = 5;
+        //    bool result = IsTrue(param);
+
+        //    if (result == true)
+        //    {
+        //        Console.WriteLine("True");
+        //    }
+        //}
+
+        //static bool IsTrue(int param)
+        //{
+        //    if (param > 0 && param < 10)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
+
+
+        //task7
+        //static void Main(string[] args)
+        //{
+        //    double num = 5.5;
+        //    double result = Square(num);
+        //    Console.WriteLine("The square of " + num + " is " + result);
+        //}
+
+        //static double Square(double num)
+        //{
+        //    return num * num;
+        //}
+
+
+
+        //task6
+        //static void Main(string[] args)
+        //{
+        //    string[] firstNames = { "Dark" };
+        //    string[] lastNames = { "Shadow" };
+
+        //    string fullName = ConcatenateArrays(firstNames, lastNames);
+
+        //    Console.WriteLine(fullName);
+        //}
+
+        //static string ConcatenateArrays(string[] firstArray, string[] secondArray)
+        //{
+        //    string concatenatedString = firstArray[0] + " " + secondArray[0];
+        //    return concatenatedString;
+        //}
+
+
+
+
+
+        //task5
+        //static void Main(string[] args)
+        //{
+        //    bool boolValue = true;
+        //    string result = GetResult(boolValue);
+        //    Console.WriteLine(result); 
+
+        //    boolValue = false;
+        //    result = GetResult(boolValue);
+        //    Console.WriteLine(result); 
+        //}
+
+        //static string GetResult(bool boolValue)
+        //{
+        //    if (boolValue)
+        //    {
+        //        return "Yes";
+        //    }
+        //    else
+        //    {
+        //        return "No";
+        //    }
+        //}
+
+
+        //task4
+        //static void Main(string[] args)
+        //{
+        //    int num = 5;
+        //    bool isEven = IsEven(num);
+        //    Console.WriteLine("{0} is even: {1}", num, isEven); 
+
+        //    num = 8;
+        //    isEven = IsEven(num);
+        //    Console.WriteLine("{0} is even: {1}", num, isEven); 
+        //}
+
+        //static bool IsEven(int num)
+        //{
+        //    if (num % 2 == 0)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
